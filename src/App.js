ddfc70b7aa9ecf911da02d2e00da0fd1aecc1eb1
Stanimir Raykov <mirkokr2004@gmail.com>
2025-06-11 20:38:23 +0300

    added popup which alerts the customer that an item is added to the cart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "./styles.css";
+import "./cartStyles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
@@ -27,6 +28,7 @@ function App() {
   );
   const [cart, setCart] = useState([]);
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
     fetch("movies.json")
@@ -48,116 +50,120 @@ function App() {
   };
 
   const addToCart = (movie) => {
-    setCart((prev) => {
-      const existingItem = prev.find((m) => m.id === movie.id);
-      if (existingItem) {
-        return prev.map((item) =>
-          item.id === movie.id
-            ? { ...item, quantity: (item.quantity || 1) + 1 }
-            : item
-        );
-      }
-      return [...prev, { ...movie, quantity: 1 }];
-    });
-  };
+  setCart((prev) => {
+    const existingItem = prev.find((m) => m.id === movie.id);
+    if (existingItem) {
+      return prev.map(item =>
+        item.id === movie.id 
+          ? { ...item, quantity: (item.quantity || 1) + 1 }
+          : item
+      );
+    }
+    return [...prev, { ...movie, quantity: 1 }];
+  });
+  
+  setShowPopup(true);
+  setTimeout(() => setShowPopup(false), 1500);
+};
 
-  return (
-    <div className="App">
-      <div className="container">
-        <Header></Header>
+    return (
+      <div className="App">
+        <div className="container">
+          <Header></Header>
 
-        <Router>
-          <nav>
-            <ul>
-              {loggedIn && (
-                <>
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/watchlist">Watchlist</Link>
-                  </li>
-                  <li>
-                    <button onClick={() => setIsCartOpen(true)}>Cart</button>
-                  </li>
-                  <li>
-                    <button onClick={handleLogout}>Logout</button>
-                  </li>
-                </>
-              )}
-              {!loggedIn && (
-                <>
-                  <li>
-                    <Link to="/loginpage">Login</Link>
-                  </li>
-                  <li>
-                    <Link to="/registerpage">Register</Link>
-                  </li>
-                </>
-              )}
-            </ul>
-          </nav>
+          <Router>
+            <nav>
+              <ul>
+                {loggedIn && (
+                  <>
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/watchlist">Watchlist</Link>
+                    </li>
+                    <li>
+                      <button onClick={() => setIsCartOpen(true)}>Cart</button>
+                    </li>
+                    <li>
+                      <button onClick={handleLogout}>Logout</button>
+                    </li>
+                  </>
+                )}
+                {!loggedIn && (
+                  <>
+                    <li>
+                      <Link to="/loginpage">Login</Link>
+                    </li>
+                    <li>
+                      <Link to="/registerpage">Register</Link>
+                    </li>
+                  </>
+                )}
+              </ul>
+            </nav>
 
-          <Routes>
-            <Route
-              path="/"
-              element={
-                loggedIn ? (
-                  <MoviesGrid
-                    watchlist={watchlist}
-                    movies={movies}
-                    toggleWatchlist={toggleWatchlist}
-                    addToCart={addToCart}
-                  />
-                ) : (
-                  <Navigate to="/loginpage" />
-                )
-              }
-            />
-            <Route
-              path="/watchlist"
-              element={
-                loggedIn ? (
-                  <Watchlist
-                    watchlist={watchlist}
-                    movies={movies}
-                    toggleWatchlist={toggleWatchlist}
-                    addToCart={addToCart}
-                  />
-                ) : (
-                  <Navigate to="/loginpage" />
-                )
-              }
-            />
-            <Route path="/movies/:id" element={<MovieDetail />} />
-            <Route
-              path="/loginpage"
-              element={
-                loggedIn ? (
-                  <Navigate to="/" />
-                ) : (
-                  <LoginPage onLogin={() => setLoggedIn(true)} />
-                )
-              }
-            />
-            <Route
-              path="/registerpage"
-              element={loggedIn ? <Navigate to="/" /> : <RegisterPage />}
-            />
-          </Routes>
-        </Router>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  loggedIn ? (
+                    <MoviesGrid
+                      watchlist={watchlist}
+                      movies={movies}
+                      toggleWatchlist={toggleWatchlist}
+                      addToCart={addToCart}
+                    />
+                  ) : (
+                    <Navigate to="/loginpage" />
+                  )
+                }
+              />
+              <Route
+                path="/watchlist"
+                element={
+                  loggedIn ? (
+                    <Watchlist
+                      watchlist={watchlist}
+                      movies={movies}
+                      toggleWatchlist={toggleWatchlist}
+                      addToCart={addToCart}
+                    />
+                  ) : (
+                    <Navigate to="/loginpage" />
+                  )
+                }
+              />
+              <Route path="/movies/:id" element={<MovieDetail />} />
+              <Route
+                path="/loginpage"
+                element={
+                  loggedIn ? (
+                    <Navigate to="/" />
+                  ) : (
+                    <LoginPage onLogin={() => setLoggedIn(true)} />
+                  )
+                }
+              />
+              <Route
+                path="/registerpage"
+                element={loggedIn ? <Navigate to="/" /> : <RegisterPage />}
+              />
+            </Routes>
+          </Router>
+        </div>
+
+        <CartSidebar
+          cart={cart}
+          setCart={setCart}
+          isOpen={isCartOpen}
+          onClose={() => setIsCartOpen(false)}
+        />
+
+        {showPopup && <div className="simple-popup">Added to Cart</div>}
+
+        <Footer></Footer>
       </div>
-
-      <CartSidebar
-        cart={cart}
-        setCart={setCart}
-        isOpen={isCartOpen}
-        onClose={() => setIsCartOpen(false)}
-      />
-
-      <Footer></Footer>
-    </div>
-  );
-}
-
+    );
+  };
 export default App;
